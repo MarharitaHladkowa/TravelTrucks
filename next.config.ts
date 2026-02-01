bf51b65b3a47acd28904589fc29a123c93e98493
Margarita Gladkowa <margarita198700@gmail.com>
2026-02-01 17:03:06 +0100

    jj

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true, // ПРАВИЛЬНО: Применяется ко всем изображениям в проекте
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "ftp.goit.study", // Разрешаем этот хост из ошибки
+        hostname: "ftp.goit.study",
         port: "",
         pathname: "/img/campers-test-task/**",
       },
